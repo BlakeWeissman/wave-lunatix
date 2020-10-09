a622c675fc36d15012d399b4ab75ae10d82573c3
Blake Weissman <contact@blakeweissman.com>
2020-10-09 16:27:58 -0400

    Implemented basic home layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import {NavLink, BrowserRouter} from 'react-router-dom';
 import {Hero} from './components/Hero/Hero';
+import {LargeInput} from './components/LargeInput/LargeInput';
 
 function App() {
   return (
@@ -21,6 +22,55 @@ function App() {
         </div>
       </div>
       <Hero title="TEST" imgFilename="logo.png" />
+      <div id="categories">
+        {
+          Array.from(Array(6), (e, i) => {
+            return (
+              <div key={i}>
+                Category {i}
+              </div>
+            );
+          })
+        }
+      </div>
+      <div className="hr-container">
+        <hr/>
+      </div>
+      <div className="cta">
+        <h3>
+          Trending
+        </h3>
+        <button>
+          View All
+        </button>
+      </div>
+      <div id="events">
+        {
+          Array.from(Array(4), (e, i) => {
+            return (
+              <div key={i}>
+                Event {i}
+              </div>
+            );
+          })
+        }
+      </div>
+      <div id="video-container">
+        <img src={require('./assets/video.png')} />        
+        <button>
+          Learn More
+        </button>
+      </div>
+      <footer>
+        <div id="apps">
+          <img src={require('./assets/apple.jpg')} />        
+          <img src={require('./assets/google.jpg')} />        
+        </div>
+        <LargeInput />
+        <p>
+          About | Support | Terms And Conditions <br /> Copyright &#169; 2020 SI Tickets | All Rights Reserved
+        </p>
+      </footer>
     </BrowserRouter>
   );
 }
