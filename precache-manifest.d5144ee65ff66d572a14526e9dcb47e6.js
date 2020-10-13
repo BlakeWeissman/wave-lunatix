5c1af0d8ce93e847424a9c2e6fbda84a38ba2e69
Blake Weissman <contact@blakeweissman.com>
2020-10-13 19:04:56 -0400

    Updates

---
+++ precache-manifest.d5144ee65ff66d572a14526e9dcb47e6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2f089ed948d8663ce9e414aab9bc8a",
+    "url": "/wave-lunatix/index.html"
+  },
+  {
+    "revision": "bc75cf0e32f174741aeb",
+    "url": "/wave-lunatix/static/css/main.43c301a4.chunk.css"
+  },
+  {
+    "revision": "66833989e535e5ff8558",
+    "url": "/wave-lunatix/static/js/2.351d8a01.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/wave-lunatix/static/js/2.351d8a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc75cf0e32f174741aeb",
+    "url": "/wave-lunatix/static/js/main.be6ef792.chunk.js"
+  },
+  {
+    "revision": "2811f2da7cadb98cc94c",
+    "url": "/wave-lunatix/static/js/runtime-main.3900e29c.js"
+  },
+  {
+    "revision": "5f1958bc17ecaac6ce2d692e3d22738d",
+    "url": "/wave-lunatix/static/media/apple.5f1958bc.jpg"
+  },
+  {
+    "revision": "43ef0c46df74b84235e2137581752324",
+    "url": "/wave-lunatix/static/media/google.43ef0c46.jpg"
+  },
+  {
+    "revision": "193f3e6bb5aa0f0098280e1c06af6452",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-700.193f3e6b.ttf"
+  },
+  {
+    "revision": "214cbcc6fad7b38840e85db9fead3feb",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-700.214cbcc6.woff"
+  },
+  {
+    "revision": "3a884322b11c29ccdfe72ad7358ce011",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-700.3a884322.woff2"
+  },
+  {
+    "revision": "3b7be8e942182c83d96f2d0026aa0c6a",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-700.3b7be8e9.eot"
+  },
+  {
+    "revision": "47de3391fe262373497d54a0cb8ce5e4",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-700.47de3391.svg"
+  },
+  {
+    "revision": "178fcf1e13369bff911a49f414ebe4dc",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-regular.178fcf1e.ttf"
+  },
+  {
+    "revision": "19ab0e7a21c016575051a08ec523acea",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-regular.19ab0e7a.woff2"
+  },
+  {
+    "revision": "7b01e1ab5348ef4fbdbf8b7b2eee4f4d",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-regular.7b01e1ab.svg"
+  },
+  {
+    "revision": "a30c1d13461a1cc46e7348b7529f1b42",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-regular.a30c1d13.woff"
+  },
+  {
+    "revision": "a713684e6a9e0faaf4bbf79fb30d5c4c",
+    "url": "/wave-lunatix/static/media/padauk-v6-latin-regular.a713684e.eot"
+  },
+  {
+    "revision": "5c87a83a9ea11071772cc0be66c88e9b",
+    "url": "/wave-lunatix/static/media/purple-short.5c87a83a.jpg"
+  },
+  {
+    "revision": "fc4a75252685e54ec1fd7c7b64aa73c6",
+    "url": "/wave-lunatix/static/media/purple.fc4a7525.jpg"
+  },
+  {
+    "revision": "e4a584ef7b392b5d2b6d2cc776e64816",
+    "url": "/wave-lunatix/static/media/sports.e4a584ef.jpg"
+  },
+  {
+    "revision": "9063b99939db805249391b2fba739792",
+    "url": "/wave-lunatix/static/media/video.9063b999.mov"
+  },
+  {
+    "revision": "caeff4edd918368b37cde3126b2ceee0",
+    "url": "/wave-lunatix/static/media/video.caeff4ed.png"
+  }
+]);
